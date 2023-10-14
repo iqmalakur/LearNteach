e2f44cb9d2ebf7c842bf15c85c5c76cf59716cc6
iqmalakur <iakbarkurnia@gmail.com>
2023-10-14 21:11:25 +0700

    User Controller

--- app/router.js
+++ app/router.js
@@ -16,4 +16,11 @@ router.post('/auth/recovery', auth.recovery.submit);
 router.get('/', home.index);
 router.get('/faq', home.faq);
 
+// User
+router.get('/user', user.index);
+router.get('/user/profile', user.profile);
+router.put('/user/profile', user.update);
+router.get('/user/wishlist', user.wishlist.show);
+router.post('/user/wishlist', user.wishlist.add);
+
 module.exports = router;
