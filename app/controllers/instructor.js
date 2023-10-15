d000abf5eda9cb6992198b0d31073c74b7bd3676
iqmalakur <iakbarkurnia@gmail.com>
2023-10-15 20:28:32 +0700

    Instructor Controller

---
+++ app/controllers/instructor.js
@@ -0,0 +1,49 @@
+module.exports = {
+  index: (req, res) => {
+    res.render('instructor/index', {
+      layout: 'layouts/main-layout',
+      title: 'Instructor - Dashboard',
+    });
+  },
+  register: {
+    show: (req, res) => {
+      res.render('instructor/register', {
+        layout: 'layouts/main-layout',
+        title: 'Register',
+      });
+    },
+    submit: (req, res) => {
+      res.send('ok');
+    },
+  },
+  course: {
+    detail: (req, res) => {
+      res.render('instructor/course', {
+        layout: 'layouts/main-layout',
+        title: 'Course',
+      });
+    },
+    content: {
+      show: (req, res) => {
+        res.render('instructor/content', {
+          layout: 'layouts/main-layout',
+          title: 'Add Content',
+        });
+      },
+      submit: (req, res) => {
+        res.send('ok');
+      },
+    },
+    quiz: {
+      show: (req, res) => {
+        res.render('instructor/quiz', {
+          layout: 'layouts/main-layout',
+          title: 'Add Quiz',
+        });
+      },
+      submit: (req, res) => {
+        res.send('ok');
+      },
+    },
+  },
+};
