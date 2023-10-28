456c0ee88677b8d8bf939ebdd9c673fdaec45702
iqmalakur <iakbarkurnia@gmail.com>
2023-10-28 16:39:54 +0700

    Create User Model

---
+++ migrations/20231028091438-create-user.js
@@ -0,0 +1,28 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      username: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING(50),
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(320),
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};
