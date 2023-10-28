73f3215ac04fc9b503cbcea0d8f80ea87b4aac77
iqmalakur <iakbarkurnia@gmail.com>
2023-10-28 17:37:38 +0700

    Create Course Model

---
+++ migrations/20231028102913-create-course.js
@@ -0,0 +1,79 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      instructor: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      description: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      rating: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      members: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      tags: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      preview: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      meet_link: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      meet_time: {
+        allowNull: false,
+        type: Sequelize.TIME,
+      },
+      meet_day: {
+        allowNull: false,
+        type: Sequelize.ENUM(
+          'Sunday',
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday'
+        ),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Courses');
+  },
+};
---
+++ models/course.js
@@ -0,0 +1,45 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Course extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Course.init(
+    {
+      id: DataTypes.INTEGER,
+      instructor: DataTypes.STRING(50),
+      title: DataTypes.STRING(50),
+      description: DataTypes.STRING,
+      rating: DataTypes.INTEGER,
+      members: DataTypes.INTEGER,
+      tags: DataTypes.STRING,
+      preview: DataTypes.STRING,
+      price: DataTypes.INTEGER,
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
+      meet_link: DataTypes.STRING,
+      meet_time: DataTypes.TIME,
+      meet_day: DataTypes.ENUM(
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ),
+    },
+    {
+      sequelize,
+      modelName: 'Course',
+    }
+  );
+  return Course;
+};
