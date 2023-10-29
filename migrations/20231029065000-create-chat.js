ffde5fe41596c21efd58ad9a216cce55db1f84d9
iqmalakur <iakbarkurnia@gmail.com>
2023-10-29 13:56:19 +0700

    Add Chat Relation Model

---
+++ migrations/20231029065000-create-chat.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Chats', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+        references: {
+          model: 'Users',
+          key: 'username',
+        },
+      },
+      community: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Communities',
+          key: 'id',
+        },
+      },
+      chat: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      chat_date: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Chats');
+  },
+};
