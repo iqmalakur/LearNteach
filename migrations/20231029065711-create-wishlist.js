36164ff6301e3de682514aa125909b612db41c5d
iqmalakur <iakbarkurnia@gmail.com>
2023-10-29 13:59:50 +0700

    Create Wishlist and Cart Relational Model

---
+++ migrations/20231029065711-create-wishlist.js
@@ -0,0 +1,33 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Wishlists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+        references: {
+          model: 'Users',
+          key: 'username',
+        },
+      },
+      course: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Courses',
+          key: 'id',
+        },
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Wishlists');
+  },
+};
