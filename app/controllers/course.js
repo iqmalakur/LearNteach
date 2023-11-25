94580de1f7ab3c550afee28998e93c54022d591e
iqmalakur <iakbarkurnia@gmail.com>
2023-11-25 16:57:14 +0700

    Course Detail

--- app/controllers/course.js
+++ app/controllers/course.js
@@ -1,4 +1,10 @@
-const { User, Instructor, Course } = require("../models/Database");
+const sequelize = require("sequelize");
+const {
+  User,
+  Instructor,
+  Course,
+  EnrolledCourse,
+} = require("../models/Database");
 const { priceFormat } = require("../utils/format");
 
 module.exports = {
@@ -36,14 +42,58 @@ module.exports = {
    * @param {Response} res The Response object.
    */
   detail: async (req, res) => {
-    const course = await Course.findByPk(req.params.courseId);
     const user = res.locals.user;
+    const course = await Course.findOne({
+      where: { id: req.params.courseId },
+      include: [
+        {
+          model: User,
+          attributes: ["name", "picture"],
+          include: [
+            {
+              model: Instructor,
+              attributes: ["rating"],
+            },
+          ],
+        },
+      ],
+    });
+
+    const enrolledCourse = await EnrolledCourse.findOne({
+      where: { user: user.username, course: course.id },
+      attributes: ["id"],
+    });
+
+    const instructorCourses = await Course.findAll({
+      where: {
+        instructor: course.instructor,
+        id: { [sequelize.Op.ne]: course.id },
+      },
+      limit: 3,
+    });
+
+    const instructorCourseTotal = await Course.findOne({
+      where: { instructor: course.instructor },
+      attributes: [[sequelize.fn("COUNT", sequelize.col("*")), "total_course"]],
+    });
+
+    const instructorStudentTotal = await Course.findOne({
+      where: { instructor: course.instructor },
+      attributes: [
+        [sequelize.fn("SUM", sequelize.col("members")), "total_student"],
+      ],
+    });
 
     res.render("course/detail", {
       layout: "layouts/main-layout",
       title: "Course Detail",
       course,
+      priceFormat,
       user,
+      enrolled: enrolledCourse ? true : false,
+      instructorCourses,
+      totalCourse: instructorCourseTotal.dataValues.total_course,
+      totalStudent: instructorStudentTotal.dataValues.total_student,
     });
   },
 
